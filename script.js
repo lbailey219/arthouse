ebf0a35a1a3cc536ea94673717b20fa95d9a791a
Lewis Bailey <lbaileyjr219@gmail.com>
2019-04-11 20:45:33 -0400

    modal

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function getZipcode(zipcode) {
-  // debugger
-  console.log(zipcode);
-  $('#modal--zipcode').val(zipcode);
-}
+// function openModal() {
+//   $('#exampleModal').modal('show');
+// }
 
 function formSubmit(name) {
   var serializedData = $(document.forms[name]).serializeObject();
+  debugger
   var url = 'https://script.google.com/macros/s/AKfycbz8vrNJ6aHq3d4p4IzXQMSshNbFx7WM9tzyyGFA9ULVzIiFFjo/exec';
   var jqxhr = $.ajax({
     url,
@@ -21,11 +20,7 @@ function formSubmit(name) {
 
 $(document).ready(function() {
   // evnt handlers
-  $('#order--here').click(function(){
-    var zipcode = $('#zipcode').val();
-
-    getZipcode(zipcode);
-  });
+  // $('#order--here').click(openModal);
 
   $('[rel="form--quote"]').submit(function(e){ // [name="form--quote"]
     e.preventDefault();
